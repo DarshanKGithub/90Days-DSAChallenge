3a8d9dff74a09f811ceecf3074f1ae4cbf82dc72
Darshan Kshetri <126147538+DarshanKGithub@users.noreply.github.com>
2025-10-30 18:28:35 +0530

    Add files via upload

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,34 @@
+package Sorting;
+
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i < n - 1; i++){
+           int minPos = i;
+            for(int j = i+1; j < n-1; j++){
+                if(arr[minPos] > arr[j]){
+                    minPos = j;
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+
+    }
+
+    public static void printSelectionSort(int arr[]){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 4, 3, 1, 0, 8, 10, 15, 16, 17};
+        selectionSort(arr);
+        printSelectionSort(arr);
+
+    }
+}
