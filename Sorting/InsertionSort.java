14869561ccaa086c97a404d47bf50e6dddca5a90
DarshanKGithub <darshankshetri8@gmail.com>
2025-10-31 09:42:51 +0530

    Insertion Sort Fundamentals

---
+++ Sorting/InsertionSort.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            arr[prev + 1] = curr;
+        }
+    }
+
+    public static void printInsertionSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 4, 3, 1, 0, 8, 10, 15, 16, 17};
+        insertionSort(arr);
+        printInsertionSort(arr);
+    }
+}
